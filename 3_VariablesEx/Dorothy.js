efe888e252ae1155146ba07596673bad13805d5a
Nihil <35506793+Nihilnia@users.noreply.github.com>
2019-11-22 23:46:56 +0300

    var average = (Number(Note01) + Number(Note02)) / 2 ;

---
+++ 3_VariablesEx/Dorothy.js
@@ -0,0 +1,11 @@
+var name;
+var note01;
+var note02;
+
+name = prompt("Name: ");
+note01 = prompt("Note01: ");
+note02 = prompt("Note02: ");
+
+var average = (Number(note02) + Number(note01)) / 2;
+
+console.log("Hello dear " + name + " your average point: " + average);
